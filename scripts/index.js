d21cb0823a506a1e4341d5d9733f0e5873174fe8
amilmohd155 <32008354+amilmohd155@users.noreply.github.com>
2020-05-13 09:25:34 +0530

    signup modal html and js parts completed

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+var modal = document.getElementById("signupModel");
+
+// Get the button that opens the modal
+var btn = document.getElementById("signupBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementById("close")
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+  console.log('clicked');
+  
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
