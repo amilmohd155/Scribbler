0150b100ac042871897ce1066108feef8fe2261f
amilmohd155 <32008354+amilmohd155@users.noreply.github.com>
2020-05-15 11:05:03 +0530

    create post modal completed, postlist page and styles file created

--- scripts/index.js
+++ scripts/index.js
@@ -10,6 +10,18 @@ var signinbtn = document.getElementById("signinBtn");
 
 var signinClose = document.getElementById("signinClose");
 
+var createPostModal = document.getElementById("createPostModal");
+var createPostBtn = document.getElementById("createPostBtn");
+var createClose = document.getElementById("createClose");
+
+createPostBtn.onclick = function () {
+  createPostModal.style.display = "block";
+};
+
+createClose.onclick = function () {
+  createPostModal.style.display = "none";
+};
+
 signupbtn.onclick = function () {
   signupmodal.style.display = "block";
 };
@@ -32,6 +44,9 @@ window.onclick = function (event) {
   }
   if (event.target == signinmodal) {
     this.signinmodal.style.display = "none";
+  }
+  if (event.target == this.createPostModal) {
+    this.createPostModal.style.display = "none";
   }
 };
 
