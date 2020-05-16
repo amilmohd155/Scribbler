df73e00fadb0a3aa26ee45de8c278329e4ebdeca
amilmohd155 <32008354+amilmohd155@users.noreply.github.com>
2020-05-16 10:59:50 +0530

    post modal completed, deleting posts working successfully

--- scripts/postslist.js
+++ scripts/postslist.js
@@ -1,10 +1,22 @@
 var trash = document.getElementsByClassName("delete-post");
 var deleteModal = document.getElementById("deleteModal");
+var trashIcon = document.getElementsByClassName("fa-trash");
+var postCard = document.getElementsByClassName("post-card");
+var element = 0;
 
-for (var i = 0; i < trash.length; ++i) {
-  trash[i].onclick = function () {
+for (let i = 0; i < trash.length; ++i) {
+  trash[i].onclick = function (event) {
     deleteModal.style.display = "flex";
+    getElementIndex(event);
   };
+}
+
+function getElementIndex(event) {
+  for (let i = 0; i < trash.length; ++i) {
+    if (trashIcon[i] == event.target) {
+      element = i;
+    }
+  }
 }
 
 window.onclick = function (event) {
@@ -13,4 +25,14 @@ window.onclick = function (event) {
   }
 };
 
-var yesBtn = document.getElementById
+var yesBtn = document.getElementById("yesBtn");
+var noBtn = document.getElementById("noBtn");
+
+noBtn.onclick = function () {
+  deleteModal.style.display = "none";
+};
+
+yesBtn.onclick = function () {
+  postCard[element].remove();
+  deleteModal.style.display = "none";
+};
